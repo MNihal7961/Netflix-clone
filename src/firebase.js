a3c92e3e11677b9a77e4d8c0c6328c046b3bce39
M Nihal <mnihal@Ms-MacBook-Air.local>
2024-03-20 17:34:04 +0530

    added loginsystem using firebase also completed homepage ui and fixed some bugs

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDttyEImYJUBM9xsE1IhJ8U6P2dbtxEGQY",
@@ -11,5 +12,6 @@ const firebaseConfig = {
   measurementId: "G-43FCJCZN2G"
 };
 
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const app = initializeApp(firebaseConfig);
+export const auth=getAuth(app)
+export const db=getFirestore(app)
